8e19de98bbeadae8312278ec2b96f87b0512f18e
Josue Rojas <josh.rojas25@gmail.com>
2018-10-06 17:02:46 -0700

    created seeds file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title:"My blog post #{blog}", 
+        body:"Lorem impsum  #{blog}"
+        )
+end
+
+5.times do |skill|
+    Skill.create!(
+        title:"rails #{skill}",
+        percent_utilized:15
+    )
+end
+
+9.times do |portfolio_item|
+    SPortfolio.create!(
+        title:"Portfolio #{portfolio_item}",
+        subtitle:"my great aunt sally's service",
+        body:"Lorem askjdnakjsdnaksjndaksndakjsdnakjsdnakjdnakjdnakjdn",
+        main_image:"https://via.placeholder.com/350x200",
+        thumb_image:"https://via.placeholder.com/600x400"
+    )
+end
