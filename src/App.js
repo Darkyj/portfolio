b1edbf6f5964b203d0a211f95a7cdd884a1d7312
Josue Rojas <josh.rojas25@gmail.com>
2018-10-16 23:29:05 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import Portfolio from './components/portfolio/portfolio';
 import Testimonials from  './components/testimonials/testimonials';
 import ContactUs from './components/contactus/contactus';
 import Footer from './components/footer/footer';
+import resumeData from './resumeData';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <About />
-        <Resume />
-        <Portfolio />
-        <Testimonials />
-        <ContactUs />
-        <Footer />
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
       </div>
     );
   }
 }
+
 export default App;
---
+++ src/resumeData.js
@@ -0,0 +1,121 @@
+let resumeData = {
+    "imagebaseurl":"https://techonoeticsclub.github.io/",
+    "name": "Josue Rojas",
+    "role": "DevOps Engineer",
+    "emailid": "Your emailid",
+    "skypeid": "Your skypeid",
+    "roleDescription": " Application Developer, DevOps, Solutions Architect, Database Administrator and pretty much anything else that needs to get done. Full stack engineer who appreciates an end-to-end operational understanding of systems. Always interested in how to automate things to require the least human interaction.",
+    "socialLinks":[
+        {
+          "name":"facebook",
+          "url":"http://facebook.com/",
+          "className":"fa fa-facebook"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com",
+          "className":"fa fa-twitter"
+        },
+        {
+          "name":"google-plus",
+          "url":"http://googleplus.com/",
+          "className":"fa fa-google-plus"
+        },
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/josuerojas/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/Darkyj",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://skype.com",
+          "className":"fa fa-skype"
+        }
+      ],
+    "aboutme":"A little info about me",
+    "address":"Somewhere in this world",
+    "website":"http://technoetics.in",
+    "education":[
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"AWS"
+      },
+      {
+        "skillname":"Linux"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
