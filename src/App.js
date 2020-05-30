f8713c84abcd63cb90d9f0ff84b925dda62041e9
jrojas <josue.rojas@aperiatech.com>
2020-05-29 22:04:19 -0700

    removing full fledged blog, deemed not necessary and I really don't want to put too much effort. Switching to hosting blog in Github pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Blog from "./components/blog/Blog";
 import Home from "./components/home/Main";
 
 const App = () => {
     return (
         <Switch> {/* The Switch decides which component to show based on the current URL.*/}
             <Route exact path='/' component={Home}/>
-            <Route exact path='/blog' component={Blog}/>
         </Switch>
     );
 }
